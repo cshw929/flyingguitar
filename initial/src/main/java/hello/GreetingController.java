bd2da960e6f1498c6f2e3ec316bdaeec089b599d
Shuwei Cao <Shuwei@Shuweis-MacBook-Pro.local>
2017-06-16 12:02:00 +0800

    first test

--- initial/src/main/java/hello/GreetingController.java
+++ initial/src/main/java/hello/GreetingController.java
@@ -11,9 +11,14 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+    @RequestMapping("/")
+    public String index(@RequestParam(value="signature", defaultValue="No signiture") String signature,
+                            @RequestParam(value="timestamp", defaultValue="No timestamp") String timestamp) {
+        //return new Greeting(counter.incrementAndGet(),String.format(template, name));
+        //System.out.println("解密后明文: ");
+
+
+
+        return signature+timestamp;
     }
 }
